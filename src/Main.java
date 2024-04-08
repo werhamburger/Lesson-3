274706fc0d5fb571ab6345d897ef564fb041d93e
werhamburger <awokado100501@gmail.com>
2024-04-08 19:24:36 +0200

    Operatory

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 5;
+        int c;
+        c = a%b;
+        System.out.println("Wynik modulo a%b:" + " " + c + ".");
+        System.out.printf("Wynik modulo a%%b: %d.", c );
+
+        }
+    }
